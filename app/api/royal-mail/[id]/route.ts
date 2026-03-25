ea1e1afa335b034911974a80477514a6b00b01d2
shijisan <cjsantos2411638@gmail.com>
2026-03-26 05:25:52 +0800

    3-26-26: fix missing nextreq in arg

--- app/api/royal-mail/[id]/route.ts
+++ app/api/royal-mail/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     const mailPieceId = await params.id;
 
     try {
