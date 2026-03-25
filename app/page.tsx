d0a35c54b8a8c40784023bc5ffaec9e9b3426472
shijisan <cjsantos2411638@gmail.com>
2026-03-26 06:37:41 +0800

    3-26-26: make homepage for basic context and prevent 404

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Homepage(){
+    return(
+        <>
+        <span>this is a proxy app, just access 'https://matchafuel-order-tracking-proxy-z93d-74j260dd1.vercel.app/api/royal-mail/(tracking_id)'</span>
+        <span>additional info at royal mail documentation site</span>
+        </>
+    )
+}
